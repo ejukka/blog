cd197d489111285116b73fea9f7a9e46d3f61c99
Jukka Edvardsson <jukka.edvardsson@gmail.com>
2015-09-27 19:50:32 +0300

    fixed broken tests

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,12 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import utils.Config
 
-object Application extends Controller {
+object Application extends Controller with Config {
   
   def index = Action {
-    Ok(views.html.index())
+    Ok(views.html.index(conf("blog.user.name")))
   }
 
 }
--- test/junit/ConfigTest.scala
+++ test/junit/ConfigTest.scala
@@ -3,19 +3,28 @@ package junit
 import org.junit.Test
 import utils.Config
 import org.junit.Assert
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.mvc.Controller
+import play.api.mvc.SimpleResult._
+import play.api.libs.json.Json._
+import play.api.Play
+import utils.Config
 
-class ConfigTest extends Config {
+class ConfigTest extends Specification with Config {
+  running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
 
-  @Test def getConfigNotFound() {
-    val notFound = conf("testi")
-    Assert.assertNotNull(notFound)
-    Assert.assertEquals("notFound", notFound)
+    @Test def getConfigNotFound() {
+      val notFound = conf("testi")
+      Assert.assertNotNull(notFound)
+      Assert.assertEquals("notFound", notFound)
+    }
+
+    @Test def getConfigFound() {
+      val found = conf("test.application")
+      Assert.assertNotNull(found)
+      Assert.assertEquals("not-test", found)
+    }
   }
-  
-  @Test def getConfigFound() {
-    val found = conf("test.application")
-    Assert.assertNotNull(found)
-    Assert.assertEquals("not-test", found)
-  }
-
 }
