1e0918216fa80a3b001adc345bc4028619cab36e
Jukka Edvardsson <jukka.edvardsson@gmail.com>
2015-09-28 09:20:18 +0300

    added simple hardcoded json response

--- app/services/impl/APIServiceImpl.scala
+++ app/services/impl/APIServiceImpl.scala
@@ -3,11 +3,19 @@ package services.impl
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+import play.api.libs.json.Json
+import play.api.libs.json.Json.toJsFieldJsValueWrapper
 import services.APIService
+import utils.Config
 
-class APIServiceImpl extends APIService {
+class APIServiceImpl extends APIService with Config {
 
   def getAPIDescription(): scala.concurrent.Future[String] = {
-    Future("api")
+    val json = Json.obj(
+      "blog" -> conf("blog.user.name"),
+      "text" -> "title text",
+      "links" -> "list of links")
+    
+    Future(json.toString)
   }
 }
