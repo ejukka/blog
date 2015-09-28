3ed36f0f5f698aef0ce38a48b33d2489e9d1e67a
Jukka Edvardsson <jukka.edvardsson@gmail.com>
2015-09-28 11:08:29 +0300

    updated readme, added ReactiveMongoModule and also added simple regularExpressionService

--- app/services/impl/APIServiceImpl.scala
+++ app/services/impl/APIServiceImpl.scala
@@ -2,17 +2,20 @@ package services.impl
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-
 import play.api.libs.json.Json
 import play.api.libs.json.Json.toJsFieldJsValueWrapper
 import services.APIService
 import utils.Config
+import services.common.RegularExpressionService
 
-class APIServiceImpl extends APIService with Config {
+class APIServiceImpl extends APIService with Config with RegularExpressionService {
 
   def getAPIDescription(): scala.concurrent.Future[String] = {
+    
+    val userName = escapeSpecialChars(conf("blog.user.name"))
+    
     val json = Json.obj(
-      "blog" -> conf("blog.user.name"),
+      "blog" -> userName,
       "text" -> "title text",
       "links" -> "list of links")
     
