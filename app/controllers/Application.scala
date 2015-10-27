fa1496fc1eb9c994bbad012dc41e57e18f353324
Jukka Edvardsson <jukka.edvardsson@gmail.com>
2015-10-27 22:36:53 +0200

    refactored text on the own model which is used through service

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,11 +3,12 @@ package controllers
 import play.api._
 import play.api.mvc._
 import utils.Config
+import services.common.TextService
 
-object Application extends Controller with Config {
+object Application extends Controller with Config with TextService {
 
   def index = Action {
-    Ok(views.html.index(conf("blog.user.name")))
+    Ok(views.html.index(conf("blog.user.name"), getText(), conf("year")))
   }
 
   def test = Action {
